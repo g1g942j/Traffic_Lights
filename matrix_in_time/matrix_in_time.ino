53060204c3f49002766a6f69409659acfa94b818
g1g942j <89134882+g1g942j@users.noreply.github.com>
2023-08-20 17:24:24 +0700

    Add files via upload

---
+++ matrix_in_time/matrix_in_time.ino
@@ -0,0 +1,52 @@
+#include <FastLED.h>
+
+#define LED_PIN 12
+#define NUM_LEDS 8
+#define BRIGHTNESS 32
+#define LED_TYPE WS2812
+
+CRGB leds[NUM_LEDS];
+
+int state, i, h;
+int step = 5;
+int duration, last_time, start_time;
+int last_state = 0;
+
+void setup() {
+  FastLED.addLeds<LED_TYPE, LED_PIN, GRB>(leds, NUM_LEDS);
+  FastLED.setBrightness(BRIGHTNESS);
+  FastLED.show();
+  Serial.begin(9600);
+  Serial.setTimeout(10);
+  //Serial.println("Write time in milles.");
+}
+
+void draw() {
+  for (i = 0; i <= NUM_LEDS - 1; i++) {
+    if (i < (state / 255)) {
+      leds[i] = CHSV(0, 0, 255);
+    }
+    if (i == (state / 255)) {
+      leds[i] = CHSV(0, 0, state % 255);
+    }
+    if (i > (state / 255)) {
+      leds[i] = CHSV(0, 0, 0);
+    }
+  }
+  FastLED.show();
+}
+
+void loop() {
+  if (Serial.available()) {
+    duration = Serial.readString().toInt();
+    start_time = millis();
+    Serial.println(duration);
+  }
+  last_state = state;
+  state = map((millis() - start_time), 0, duration, 0, 8 * 255);
+  if ((millis() - start_time) < duration && last_state != state) {
+    draw();
+  } else {
+    Serial.println(millis() - start_time);
+  }
+}
